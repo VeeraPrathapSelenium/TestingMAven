da8a3f38f366e683be83cec507908714587f7837
tm <tm@Main>
2018-03-25 10:14:42 +0530

    1st commit

---
+++ src/test/java/com/testing/Login.java
@@ -0,0 +1,27 @@
+package com.testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	
+	@Test
+	public void HomePage()
+	{
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://testingmasters.com/hrm");
+		
+		driver.findElement(By.id("txtUsername")).sendKeys("user01");
+		
+		driver.findElement(By.id("txtPassword")).sendKeys("pass1234");
+		driver.findElement(By.id("btnLogin")).click();
+	}
+	
+	
+
+}
